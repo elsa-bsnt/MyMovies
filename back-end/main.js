b87eaee12e797a0deecc8afc4aaae7187cb3b922
elsa853 <elsa.bessonnet@epitech.digital>
2023-01-18 11:23:19 +0100

    register branch

---
+++ back-end/main.js
@@ -0,0 +1,18 @@
+let express = require('express'),
+    app = express(),
+    port = process.env.DB_PORT || 3307;
+bodyParser = require('body-parser');
+let cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let routes = require('./routes');
+routes(app);
+
+app.listen(port, () => {
+    console.log("port " + port + " connecté")
+});
+
+console.log('Listening on PORT: ' + port);
